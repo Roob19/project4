8dacc0292bb7425c46801a9848b5cfbe1fb330fb
Ruben <21150133+Roob19@users.noreply.github.com>
2022-06-14 18:37:21 -0700

    adding YELP_TOKEN to server.js and Twitter Widget attempt

--- server.js
+++ server.js
@@ -2,10 +2,12 @@ const express = require("express");
 const path = require("path");
 const favicon = require("serve-favicon");
 const logger = require("morgan");
+
 require("dotenv").config();
 require("./config/database");
 
 const app = express();
+const YELP_TOKEN = process.env.YELP_TOKEN;
 
 app.use(logger("dev"));
 app.use(express.json());
